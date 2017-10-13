4d2a0d97a336c7c361da501475ad59a4448b879f
dlill <daniel.lill@posteo.de>
2017-10-13 14:12:39 +0200

    insert tee print

--- R/addins.R
+++ R/addins.R
@@ -9,3 +9,14 @@ insert_tee_operator <- function ()
   rstudioapi::insertText(" %T>% ")
 }
 
+#' Addin to insert tee-operator
+#'
+#' @return
+#' @export
+#'
+#' @examples
+insert_tee_print <- function ()
+{
+  rstudioapi::insertText(" %T>% print ")
+}
+
