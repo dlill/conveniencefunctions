8bfe44873ec782b65e7aa3e8a7a3ba198c90cdac
Daniel Lill <daniel.lill@posteo.de>
2018-07-31 13:30:09 +0200

    FEATURE remove_geom()

--- R/tidyversetools.R
+++ R/tidyversetools.R
@@ -26,7 +26,34 @@ merge_col2_into_col1 <- function(.data, col1, col2) {
 
 
 
+# ggplot2 ----
 
+#' Remove a geom layer
+#'
+#' @param x ggplot object
+#' @param geom_type Character such as "GeomLine"
+#' @param last_only only the topmost layer of this kind is removed
+#'
+#' @details This function was mainly written by Pedro Aphalo on https://stackoverflow.com/questions/13407236/remove-a-layer-from-a-ggplot2-chart#51612148
+#' I only added the last_only - argument
+#'
+#' @return the plot with the layers removed
+#' @export
+remove_geom <- function(x, geom_type, last_only = T) {
+  # Find layers that match the requested type.
+  selector <- sapply(x$layers,
+                     function(y) {
+                       class(y$geom)[1] == geom_type
+                     })
+  if(last_only & sum(selector) > 0)
+    selector <- max(which(selector))
+  # Delete the layers.
+  x$layers[selector] <- NULL
+  x
+}
+
+
+selector <- c(T,F,T, F)
 
 
 #' ggsave() with automatically saving metainformation of how the plot was produced
