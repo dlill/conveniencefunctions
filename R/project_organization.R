358cc37bd110b6b54beb5405c971b4a4ed3d7aab
Daniel Lill <daniel.lill@posteo.de>
2019-03-25 14:40:50 +0100

    proj_create_folders

---
+++ R/project_organization.R
@@ -0,0 +1,16 @@
+#' Create the basic project folder structure
+#'
+#' @param path the path of the project folder
+#'
+#' @return this function is called for its side-effect
+#' @export
+proj_create_folders <- function(path) {
+
+    dirs <- c("Scripts", "Data", "Outputs", "Documentation")
+
+  for (d in dirs) {
+    if (!dir.exists(file.path(d)))
+      dir.create(file.path(d))
+  }
+
+}
