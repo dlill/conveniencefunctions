ab44366f29cb9cae2ca470054049080d9d60fe3e
daniel2 IQdesktop <daniel2@IQdesktop>
2020-01-07 13:22:44 +0100

    keybindings bugfix

--- R/keybindings.R
+++ R/keybindings.R
@@ -3,6 +3,8 @@
 #' @export
 install_cfkeybindings <- function(){
   keybindings_path <- "~/.R/rstudio/keybindings"
+  if (!dir.exists(keybindings_path)) dir.create(keybindings_path, FALSE, TRUE)
   keybindings_files <- list.files("inst/keybindings", "json$", F, T)
   lapply(keybindings_files, file.copy, to = keybindings_path, overwrite = TRUE)
+  "keybindings installed"
 }
