5648041f8b7d03683d78a2b9a1b12d7d69db5347
daniel2 IQdesktop <daniel2@IQdesktop>
2020-01-07 11:06:46 +0100

    install script for IQDesktop

---
+++ inst/IQDesktop/install_Software.sh
@@ -0,0 +1,5 @@
+cd
+mkdir PROJTOOLS
+git clone https://github.com/dlill/conveniencefunctions
+git clone https://github.com/IntiQuan/IQRtools
+R CMD install.packages("conveniencefunctions", repos = NULL)
