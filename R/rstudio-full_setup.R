14bf9f1429ed751adda21ca668b064a3458397c1
Daniel Lill <daniel.lill@posteo.de>
2020-01-09 11:10:14 +0100

    gitall

--- R/rstudio-full_setup.R
+++ R/rstudio-full_setup.R
@@ -13,6 +13,6 @@ install_cfrstudio <- function(theme_name = "pastel on dark") {
   # 3. Snippets
   install_cfsnippets()
   # 4. Bash alias for git
-  file.copy(system.file("bash/bash_aliases", package = "conveniencefunctions"), "~")
+  file.copy(system.file("bash/bash_aliases", package = "conveniencefunctions"), "~/.bash_aliases", overwrite = TRUE)
   
 }
