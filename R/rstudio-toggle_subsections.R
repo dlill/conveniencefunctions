1d8979d606bdc6269a7de1442f6491eb95cf074e
Daniel Lill <daniel.lill@posteo.de>
2020-07-24 13:50:04 +0200

    rename subsections

--- R/rstudio-toggle_subsections.R
+++ R/rstudio-toggle_subsections.R
@@ -1,4 +1,6 @@
 #' @rdname toggle
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 toggle_off <- function(line, text, editor) {
   d1 <- rstudioapi::document_position(line, nchar(text[line]) + 1)
   rstudioapi::modifyRange(d1, " #", editor$id)
@@ -6,6 +8,8 @@ toggle_off <- function(line, text, editor) {
 }
 
 #' @rdname toggle
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 toggle_on <- function(line, text, editor) {
   d1 <- rstudioapi::document_position(line, nchar(text[line]) - 1)
   d2 <- rstudioapi::document_position(line, nchar(text[line]) + 1)
@@ -19,15 +23,17 @@ toggle_on <- function(line, text, editor) {
 #' In the document outline, toggle a section on or off by adding or removing a #
 #'
 #' @return called for side-effect
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 toggle <- function(dashes = "----") {
   e <- rstudioapi::getSourceEditorContext()
   rstudioapi::documentSave(e$id)
   e <- rstudioapi::getSourceEditorContext()
   text <- readLines(e$path)
-
+  
   sec_on    <- grep(paste0("# (.*) ", dashes, "$"), text)
   sec_off   <- grep(paste0("# (.*) ", dashes, " #$"), text)
-
+  
   if (length(sec_off)){
     lapply(sec_off, toggle_on, text = text, editor = e)
     return(NULL)
@@ -37,19 +43,27 @@ toggle <- function(dashes = "----") {
   }
 }
 
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 #' @export
 #' @rdname toggle
 toggle_section <- function() toggle("----")
 
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 #' @export
 #' @rdname toggle
 toggle_subsection <- function() toggle("-----")
 
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 #' @rdname toggle
 #' @export
 toggle_subsubsection <- function() toggle("------")
 
 
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 #' @export
 #' @rdname toggle
 transform_subsection <- function(line, text, editor) {
@@ -68,6 +82,8 @@ transform_subsection <- function(line, text, editor) {
   NULL
 }
 
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
 #' @export
 #' @rdname toggle
 initiate_or_delete_subsection <- function(line, text, editor) {
@@ -95,6 +111,66 @@ initiate_or_delete_subsection <- function(line, text, editor) {
   rstudioapi::documentSave(e$id)
   NULL
 }
+
+
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
+#' @param FLAGfunctionAsSection Start subsections within functions with 1. Switch off if the function is just a small part of a bigger logic
+#' @export
+#' @rdname toggle
+#' @importFrom data.table data.table
+renumber_sections <- function(FLAGfunctionAsSection = TRUE) {
+  e <- rstudioapi::getSourceEditorContext()
+  text <- readLines(e$path)
+  
+  # .. 1 Get lines -----
+  s1all <- s1 <- grep("(?<!Exit) -{4}$", text, perl = TRUE)
+  if (FLAGfunctionAsSection){
+    functions <- grep("function(", text, fixed = TRUE)
+    s1all <- c(s1all, functions)
+  }
+  s2 <- grep(" -{5}$", text)
+  s3 <- grep(" -{6}$", text)
+  
+  # .. 1  # Associate subsubs to subs -----
+  if (length(s3)){
+    ds3 <- data.table::data.table(s = s3)
+    ds3[,`:=`(s2associated = which.min(s > s2) ), by = (1:nrow(ds3))]
+    ds3[,`:=`(number = 1:.N), by = s2associated]
+    for (i in 1:nrow(ds3)) {
+      line <- ds3[i,s]
+      n <- ds3[i,number]
+      reg <- regexpr("# .... \\d* ?", text[line])
+      rstudioapi::modifyRange(c(line, reg, line, reg + attr(reg, "match.length")), paste0("# .... ", n, " "))
+    }
+  }
+  # .. 2  # Associate subs to s -----
+  if (length(s2)){
+    ds2 <- data.table::data.table(s = s2)
+    ds2[,`:=`(s1associated = which.min(s > s1all) ), by = (1:nrow(ds2))]
+    ds2[,`:=`(number = 1:.N), by = s1associated]
+    for (i in 1:nrow(ds2)) {
+      line <- ds2[i,s]
+      n <- ds2[i,number]
+      reg <- regexpr("# .. \\d* ?", text[line])
+      rstudioapi::modifyRange(c(line, reg, line, reg + attr(reg, "match.length")), paste0("# .. ", n, " "))
+    }
+  }
+  
+  # .. 3 Number sections -----
+  if (length(s1)){
+    ds1 <- data.table::data.table(s = s1)
+    ds1[,`:=`(number = 1:.N - 1)]  
+    for (i in 1:nrow(ds1)) {
+      line <- ds1[i,s]
+      n <- ds1[i,number]
+      reg <- regexpr("# \\d* ?", text[line])
+      rstudioapi::modifyRange(c(line, reg, line, reg + attr(reg, "match.length")), paste0("# ", n, " "))
+    }
+  }
+  NULL
+}
+
 
 
 
--- renumberingSubsections.R
+++ renumberingSubsections.R
@@ -1,99 +1,29 @@
 # -------------------------------------------------------------------------#
-# 1 dfgh ----
+# 0 dfgh ----
 # -------------------------------------------------------------------------#
 
 # -------------------------------------------------------------------------#
-# 5 dfgh ----
+# 1 dfgh ----
 # -------------------------------------------------------------------------#
 
 # .. 1 sdf -----
 # .... 1 sdfasfgah ------
 # .... 2 sdfassdffgah ------
-# .. 3 sdfasdf -----
+# .. 2 sdfasdf -----
 # .... 1 sdfasfgah ------
 
 # -------------------------------------------------------------------------#
-# 4 jdsfg ----
+# 2 jdsfg ----
 # -------------------------------------------------------------------------#
 
-# .. sdf -----
+# .. 1 1 1sdf -----
 # .... 1 sdfasfgah ------
 # .... 2 sdfasfgah ------
 # .... 3 sdfasfdfggah ------
 
-# .. sdfgsdf -----
+# .. 2 sdfgsdf -----
+
+
+
 
 # Exit ----
-
-#' @export
-#' @rdname toggle
-renumberSections <- function(line, text, editor, FLAGfunctionAsSection = TRUE) {
-  # >> Script development. # [] Remove in final script << ----
-  FLAGfunctionAsSection = TRUE
-  e <- rstudioapi::getSourceEditorContext()
-  text <- readLines(e$path)
-  
-  # .. Get lines -----
-  s1 <- grep("(?<!Exit) -{4}$", text, perl = TRUE)
-  if (FLAGfunctionAsSection){
-    functions <- grep("function(", text, fixed = TRUE)
-    s1 <- c(s1, functions)}
-  s2 <- grep(" -{5}$", text)
-  s3 <- grep(" -{6}$", text)
-  
-  # ..   # Associate subsubs to subs -----
-  ds3 <- data.table(s = s3)
-  ds3[,`:=`(s2associated = which.min(s > s2) - 1), by = (1:nrow(ds3))]
-  ds3[,`:=`(number = 1:.N), by = s2associated]
-  
-  # ..   # Associate subs to s -----
-  ds2 <- data.table(s = s2)
-  ds2[,`:=`(s1associated = which.min(s > s1) - 1), by = (1:nrow(ds2))]
-  ds2[,`:=`(number = 1:.N), by = s1associated]
-  
-  # .. Number sections -----
-  ds1 <- data.table(s = s1)
-  ds1[,`:=`(number = 1:.N - 1)]  
-  
-  
-  # .. do renaming -----
-  i <- 1
-  for (i in 1:nrow(ds3)) {
-    line <- ds3[i,s]
-    n <- ds3[i,number]
-    reg <- regexpr("# .... \\d+", text[line])
-    rstudioapi::modifyRange(c(line, reg, line, reg + attr(reg, "match.length")), paste0("# .... ", n))
-    }
-  
-  for (i in 1:nrow(ds2)) {
-    line <- ds2[i,s]
-    n <- ds2[i,number]
-    reg <- regexpr("# .. \\d+", text[line])
-    rstudioapi::modifyRange(c(line, reg, line, reg + attr(reg, "match.length")), paste0("# .. ", n))
-    }
-  
-  
-  # ..  -----
-  
-  if (grepl(" -{6}$", linetext)) {
-    # Remove
-    rstudioapi::modifyRange(c(line, nchar(linetext)-6, line, Inf), "")
-    rstudioapi::modifyRange(c(line, 1, line, 8), "")
-  } else if (grepl(" -{5}$", linetext)) {
-    # Remove
-    rstudioapi::modifyRange(c(line, nchar(linetext)-5, line, Inf), "")
-    rstudioapi::modifyRange(c(line, 1, line, 6), "")
-  } else if (grepl("^# ", linetext)) {
-    # Turn comment into subsection
-    rstudioapi::insertText(c(line, 3), ".. ")
-    rstudioapi::insertText(c(line, Inf), " -----")
-  } else {
-    # Turn code into subsection
-    rstudioapi::insertText(c(line, 1), "# .. ")
-    rstudioapi::insertText(c(line, Inf), " -----")
-  }
-  rstudioapi::documentSave(e$id)
-  NULL
-}
-
-
