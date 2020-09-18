74a9f025d3a16e3f479624a748058478c7ec7066
Daniel Lill <daniel.lill@intiquan.com>
2020-09-18 11:39:16 +0200

    more setup IQDesktop

--- inst/setup_IQDesktop/Setup/copyResourcesToRemote.sh
+++ inst/setup_IQDesktop/Setup/copyResourcesToRemote.sh
@@ -1,3 +1,4 @@
 ssh-copy-id -p 6913 daniel2@192.168.1.11
 scp -P 6913 /c/PROJTOOLS/IQDesktop/id_rsa.zip  daniel2@192.168.1.11:~/id_rsa.zip
-scp -P 6913 /c/PROJTOOLS/conveniencefunction/inst/setup_IQDesktop/Setup/Resources/setupRemote.sh daniel2@192.168.1.11:~/setupRemote.sh
+scp -P 6913 /c/PROJTOOLS/conveniencefunctions/inst/setup_IQDesktop/Setup/Resources/setupRemote.sh daniel2@192.168.1.11:~/setupRemote.sh
+ssh -p 6913 daniel2@192.168.1.11 'cd && chmod +x setupRemote.sh'
