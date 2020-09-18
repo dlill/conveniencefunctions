65d37e48298ce09d0015e07a72a33e833c830d67
Daniel <daniel.lill@intiquan.com>
2020-09-18 12:39:46 +0200

    update to new setup

--- R/rstudio-full_setup.R
+++ R/rstudio-full_setup.R
@@ -7,31 +7,27 @@
 #' @export
 cf_install_rstudio <- function(FLAGoverwrite = TRUE, FLAGshortcuts = FALSE) {
   # 1. Theme 
-  rstudioapi::applyTheme("pastel on dark")
+  try(rstudioapi::applyTheme("pastel on dark"))
   # 2. Keybindings
   install_cfkeybindings(FLAGoverwrite = FLAGoverwrite)
   # 3. Snippets
   install_cfsnippets(FLAGoverwrite = FLAGoverwrite)  
-  wup <- FLAGoverwrite
   # 4. Bash alias for git
-  if (Sys.info()["sysname"] == "Linux"){
-    wup <- file.copy(system.file("setup_IQDesktop/bash/bash_aliases", package = "conveniencefunctions"), "~/.bash_aliases", overwrite = FLAGoverwrite) 
-    system("sed -i 's/\r//' ~/.bash_aliases")
-  }
-  if (Sys.info()["sysname"] == "Windows"){
-    wup <- file.copy(system.file("setup_IQDesktop/bash/bash_aliases", package = "conveniencefunctions"), "~/../.bash_aliases", overwrite = FLAGoverwrite) 
-    writeLines("source ~/.bash_aliases", "~/../.bashrc")
-  }
+  bashrcfile <- "~/.bashrc"
+  if (Sys.info()["sysname"] == "Windows") bashrcfile <- "~/../.bashrc"
+    newLines <- readLines(system.file("setup_IQDesktop/Setup/Resources/bash/bash_aliases", package = "conveniencefunctions")) 
+    oldLines <- readLines(bashrcfile)
+    cat(c("\n", setdiff(newLines, oldLines)), sep = "\n\n", file = bashrcfile, append = TRUE)
   if (wup) cat("Bash aliases installed \n")
   # 5. Install shortcuts for Thunar
   if (FLAGshortcuts){
     if (!dir.exists("~/.config/gtk-3.0")) dir.create("~/.config/gtk-3.0")
-    wup <- file.copy(system.file("setup_IQDesktop/thunar_shortcuts/bookmarks", package = "conveniencefunctions"), 
+    wup <- file.copy(system.file("setup_IQDesktop/Setup/Resources/thunar_shortcuts/bookmarks", package = "conveniencefunctions"), 
                      "~/.config/gtk-3.0/bookmarks", overwrite = FLAGoverwrite) 
     if (wup) cat("Explorer shortcuts installed \n")
   }
   # 6. .Rprofile
-  file.copy(system.file("setup_IQDesktop/.Rprofile"), "~/.Rprofile")
+  file.copy(system.file("setup_IQDesktop/Setup/Resources/.Rprofile"), "~/.Rprofile")
 }
 
 
@@ -43,7 +39,7 @@ cf_install_rstudio <- function(FLAGoverwrite = TRUE, FLAGshortcuts = FALSE) {
 install_cfkeybindings <- function(FLAGoverwrite = FALSE){
   keybindings_path <- "~/.R/rstudio/keybindings"
   if (!dir.exists(keybindings_path)) dir.create(keybindings_path, FALSE, TRUE)
-  keybindings_files <- list.files(system.file("setup_IQDesktop/keybindings", package = "conveniencefunctions"), "json$", F, T)
+  keybindings_files <- list.files(system.file("setup_rstudio/keybindings", package = "conveniencefunctions"), "json$", F, T)
   wup <- vapply(stats::setNames(nm = keybindings_files), file.copy, to = keybindings_path, overwrite = FLAGoverwrite, FUN.VALUE = TRUE)
   if (any(wup)) cat(paste0(names(wup)[wup], collapse = " .... \n"),  "installed\n")
   NULL
@@ -58,30 +54,8 @@ install_cfkeybindings <- function(FLAGoverwrite = FALSE){
 #' @examples
 install_cfsnippets <- function(FLAGoverwrite = FALSE){
   if(!dir.exists("~/.R/snippets/")) dir.create("~/.R/snippets/", recursive = TRUE)
-  wup <- file.copy(system.file("setup_IQDesktop/snippets/r.snippets", package = "conveniencefunctions"), file.path("~/.R/snippets/r.snippets"), overwrite = FLAGoverwrite)
+  wup <- file.copy(system.file("setup_rstudio/snippets/r.snippets", package = "conveniencefunctions"), file.path("~/.R/snippets/r.snippets"), overwrite = FLAGoverwrite)
   if (wup) cat("Snippets were overwritten \n")
   NULL
 }
 
-#' Backup the IQRdesktop setup scripts
-#'
-#' @param path_IQDesktop 
-#' @param filename_zip 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-backup_IQdesktopSetupScripts <- function(path_IQDesktop = "/IQDESKTOP/PROJTOOLS/IQDesktop/",
-                                         filename_zip = "~/PROJTOOLS/conveniencefunctions/inst/setup_IQDesktop/setup.zip") {
-  
-  files <- list.files(path_IQDesktop, all.files = TRUE, recursive = TRUE, full.names = FALSE)
-  files <- grep("id_rsa", files, invert = TRUE, value = TRUE)
-  
-  unlink(filename_zip)
-  curwd <- getwd()
-  setwd(path_IQDesktop)
-  zip(zipfile = filename_zip, files)
-  setwd(curwd)
-  
-}
