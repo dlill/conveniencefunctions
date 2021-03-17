5213b73d1228e696326ab058b8870d646d2b4593
Daniel Lill <daniel.lill@posteo.de>
2021-03-17 23:18:28 +0100

    constructors for PEtab files

--- R/PEtab.R
+++ R/PEtab.R
@@ -1,6 +1,6 @@
 
 
-#' Title
+#' Constructor for Conditions
 #'
 #' https://petab.readthedocs.io/en/stable/documentation_data_format.html#condition-table
 #'
@@ -16,59 +16,119 @@ petab_condition <- function(
   conditionId,
   conditionName = NULL,
   ...) {
-  data.table(conditionId = conditionId, conditionName = conditionName, as.data.table(list(...)))
+  data.table(conditionId = conditionId, 
+             conditionName = conditionName, 
+             as.data.table(list(...)))
 }
 
 
+#' Constructor for Measurements 
+#' 
+#' @param observableId 
+#' @param simulationConditionId 
+#' @param measurement 
+#' @param time 
 #' @param observableParameters numeric string or null
+#' @param datasetId 
+#' @param replicateId 
+#' @param preequilibrationConditionId 
 #' @param noiseParameters numeric, string or null: Measurement noise or parameter name
 petab_measurements <- function(
-observableId,
-preequilibrationConditionId = NULL,
-simulationConditionId,
-measurement,
-time,
-observableParameters = list(NULL, "1;1", "scale_i;offset_i")[[1]], 
-noiseParameters = list(1, "error_obsi_ADD;error_obsi_REL")[[1]],
-datasetId = NULL,
-replicateId = NULL) {
-data.table(
-  observableId = observableId,
-  preequilibrationConditionId = preequilibrationConditionId,
-  simulationConditionId = simulationConditionId,
-  measurement = measurement,
-  time = time,
-  observableParameters = observableParameters,
-  noiseParameters = noiseParameters,
-  datasetId = datasetId,
-  replicateId = replicateId)
+  observableId,
+  simulationConditionId,
+  measurement,
+  time,
+  observableParameters        = list(NULL, "1;1", "scale_i;offset_i")[[1]], 
+  noiseParameters             = list(1, "error_obsi_ADD;error_obsi_REL")[[1]],
+  datasetId                   = NULL,
+  replicateId                 = NULL,
+  preequilibrationConditionId = NULL
+) {
+  data.table(
+    observableId                = observableId,
+    preequilibrationConditionId = preequilibrationConditionId,
+    simulationConditionId       = simulationConditionId,
+    measurement                 = measurement,
+    time                        = time,
+    observableParameters        = observableParameters,
+    noiseParameters             = noiseParameters,
+    datasetId                   = datasetId,
+    replicateId                 = replicateId)
 }
 
-# For noiseDistribution, normal and laplace are supported. For observableTransformation, lin, log and log10 are supported. Denote by 𝑦 the simulation, 𝑚 the measurement, and 𝜎 the standard deviation of a normal, or the scale parameter of a laplace model, as given via the noiseFormula field. Then we have the following effective noise distributions.
+#' Constructor for Observables
+#'
+#' @param observableId 
+#' @param observableName 
+#' @param observableFormula 
+#' @param observableTransformation 
+#' @param noiseFormula 
+#' @param noiseDistribution 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 petab_observables <- function(
   observableId,
-  observableName = NULL,
- 	observableFormula = "observableParameter${n}_${observableId} * state1",
-  observableTransformation = c("lin", "log", "log10"),
- 	noiseFormula = c(1, "noiseParameter${n}_${observableId}"), # aka errormodel
-  noiseDistribution = c("normal", "laplace")) {
-
+  observableName           = NULL,
+  observableFormula        = "observableParameter${n}_${observableId} * state1",
+  observableTransformation = c("lin", "log", "log10")[[1]],
+  noiseFormula             = c(1, "noiseParameter${n}_${observableId}")[[1]], # aka errormodel
+  noiseDistribution        = c("normal", "laplace")[[1]]) {
+  data.table(
+    observableId             = observableId,
+    observableName           = observableName,
+    observableFormula        = observableFormula,
+    observableTransformation = observableTransformation,
+    noiseFormula             = noiseFormula,
+    noiseDistribution        = noiseDistribution
+  )
 }
 
-# petab_parameter
-petab_parameter <- function(
-parameterId,
-parameterName = NULL,
-parameterScale = c("log", "lin", "log10"),
-lowerBound = 0,     # given on linear scale
-upperBound = 10000, # given on linear scale
-nominalValue = 1,   # given on linear scale
-estimate = c(0,1),
-initializationPriorType = c("parameterScaleUniform","uniform","normal","laplace","logNormal","logLaplace","parameterScaleNormal","parameterScaleLaplace"),
-initializationPriorParameters = "-1;1",
-objectivePriorType = c("parameterScaleNormal","parameterScaleUniform","uniform","normal","laplace","logNormal","logLaplace","parameterScaleLaplace"),
-objectivePriorParameters = "-1;1") {
-
+#' Constructor for Parameters
+#'
+#' @param parameterId 
+#' @param parameterName 
+#' @param parameterScale 
+#' @param lowerBound 
+#' @param upperBound 
+#' @param nominalValue 
+#' @param estimate 
+#' @param initializationPriorType 
+#' @param initializationPriorParameters 
+#' @param objectivePriorType 
+#' @param objectivePriorParameters 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+petab_parameters <- function(
+  parameterId,
+  parameterName                 = NULL,
+  parameterScale                = c("log", "lin", "log10")[[1]],
+  lowerBound                    = 0,     # given on linear scale
+  upperBound                    = 10000, # given on linear scale
+  nominalValue                  = 1,   # given on linear scale
+  estimate                      = c(1,0)[[1]],
+  initializationPriorType       = c("parameterScaleUniform","uniform","normal","laplace","logNormal","logLaplace","parameterScaleNormal","parameterScaleLaplace")[[1]],
+  initializationPriorParameters = "-1;1",
+  objectivePriorType            = c("parameterScaleNormal","parameterScaleUniform","uniform","normal","laplace","logNormal","logLaplace","parameterScaleLaplace")[[1]],
+  objectivePriorParameters      = "-1;1") {
+  data.table(
+    parameterId                   = parameterId,
+    parameterName                 = parameterName,
+    parameterScale                = parameterScale,
+    lowerBound                    = lowerBound,
+    upperBound                    = upperBound,
+    nominalValue                  = nominalValue,
+    estimate                      = estimate,
+    initializationPriorType       = initializationPriorType,
+    initializationPriorParameters = initializationPriorParameters,
+    objectivePriorType            = objectivePriorType,
+    objectivePriorParameters      = objectivePriorParameters
+  )
 }
 
 
