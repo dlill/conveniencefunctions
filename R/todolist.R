5e405b3ecc73287bdd62b1ee93d2df489e0a25c6
Daniel Lill <daniel.lill@posteo.de>
2021-05-27 12:33:46 +0200

    import copy in todolist

--- R/todolist.R
+++ R/todolist.R
@@ -9,7 +9,7 @@
 #' @author Daniel Lill (daniel.lill@physik.uni-freiburg.de)
 #' @md
 #' @family 
-#' @importFrom data.table data.table rbindlist
+#' @importFrom data.table data.table rbindlist copy
 #' @importFrom stringr str_extract str_remove str_detect fixed
 todolist <- function(path = ".", FLAGrecursive = FALSE, FLAGremoveDone = TRUE) {
   
@@ -21,14 +21,14 @@ todolist <- function(path = ".", FLAGrecursive = FALSE, FLAGremoveDone = TRUE) {
   
   d <- data.table::rbindlist(d, idcol = "script")
   
-  d[,`:=`(scriptn = copy(script))]
+  d[,`:=`(scriptn = data.table::copy(script))]
   d[,`:=`(scriptn = stringr::str_extract(scriptn, "S\\d+(-\\d+)?"))]
   d[,`:=`(scriptn = stringr::str_remove(scriptn, "S"))]
   
   d[,`:=`(done = stringr::str_detect(gout, "\\[x\\]"))]
   d[,`:=`(check = ifelse(done, "[x]", "[ ]"))]
   
-  d[,`:=`(task = copy(gout))]
+  d[,`:=`(task = data.table::copy(gout))]
   d[,`:=`(task = stringr::str_remove(task, "^# ?"))]
   d[,`:=`(task = stringr::str_remove(task, stringr::fixed(check)))]
   
